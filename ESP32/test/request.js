882763e3400c5ed5682d03e55e591933e5b8dfc6
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-12-13 15:45:45 +0800

    feat: update the test

--- ESP32/test/request.js
+++ ESP32/test/request.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 
-const url = "http://192.168.137.166/test2"
+// const url = "http://192.168.137.29/test2"
+const url = "https://51967l20e0.yicp.fun/test2"
 
-// axios.get(url)
-//     .then((res) => {
-//         console.log(res.data);
-//     })
+const argv = process.argv;
 
 axios.post(url, {
-    command: "GO",
+    command: argv[2],
 }).then((res) => {
     console.log(res.data);
+}).catch((err) => {
+    console.log(err);
 })
